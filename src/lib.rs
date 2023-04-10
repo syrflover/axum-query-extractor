7e9a0bb44da6f288de4c5381a0edccecf7f865a6
syr <user@syrflover.co>
2023-04-10 15:48:43 +0900

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+use std::borrow::Cow;
+
+use axum::{
+    body,
+    extract::FromRequestParts,
+    http::{request::Parts, StatusCode},
+    response::{IntoResponse, Response},
+};
+use serde::de::DeserializeOwned;
+
+#[derive(Debug, thiserror::Error)]
+pub enum QueryRejection {
+    #[error("Deserialize querystring: {0}")]
+    Deserialize(serde_qs::Error),
+}
+
+impl IntoResponse for QueryRejection {
+    fn into_response(self) -> axum::response::Response {
+        Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(body::boxed(self.to_string()))
+            .unwrap()
+    }
+}
+
+pub struct Query<T>(pub T);
+
+impl<T> TryFrom<&str> for Query<T>
+where
+    T: DeserializeOwned,
+{
+    type Error = QueryRejection;
+
+    fn try_from(query: &str) -> Result<Self, Self::Error> {
+        let value = serde_qs::from_str(query).map_err(QueryRejection::Deserialize)?;
+        Ok(Query(value))
+    }
+}
+
+impl<'a, T> TryFrom<Cow<'a, str>> for Query<T>
+where
+    T: DeserializeOwned,
+{
+    type Error = QueryRejection;
+
+    fn try_from(query: Cow<str>) -> Result<Self, Self::Error> {
+        match query {
+            Cow::Borrowed(query) => query.try_into(),
+            Cow::Owned(query) => query.as_str().try_into(),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl<T, S> FromRequestParts<S> for Query<T>
+where
+    T: DeserializeOwned,
+    S: Send + Sync,
+{
+    type Rejection = QueryRejection;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let query = parts
+            .uri
+            .query()
+            .and_then(|query| urlencoding::decode(query).ok())
+            .unwrap_or_default();
+        query.try_into()
+    }
+}
